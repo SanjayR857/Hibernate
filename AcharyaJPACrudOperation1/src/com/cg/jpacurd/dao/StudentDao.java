1b2d2998378b5c203ee3b5ad802bd1db65f88241
SanjayR857 <sanjayr9586@gmail.com>
2022-08-11 10:38:19 +0530

    FIRST COMMIT

---
+++ AcharyaJPACrudOperation1/src/com/cg/jpacurd/dao/StudentDao.java
@@ -0,0 +1,14 @@
+package com.cg.jpacurd.dao;
+
+	import com.cg.jpacurd.entities.*;
+	
+	public interface StudentDao {
+	public abstract Student getStudentById(int id);
+	public abstract void addStudent(Student student);
+	public abstract void removeStudent(Student student);
+	public abstract void updateStudent(Student student);
+
+	public abstract void commitTransaction();
+	public abstract void beginTransaction();
+	}
+
